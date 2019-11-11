115bea1eb643bc0ce405accdacc5cac58cbd337a
Tareq Islam <mti.tareq3@gmail.com>
2019-11-11 21:22:02 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final title;
+
+  const MyHomePage({Key key, this.title}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _getCounter().then((value) => {
+      
+      _counter=value
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                    child: Text("Increase the counter"),
+                    onPressed: () => {increamentCounter()}),
+                Text('Counter $_counter '),
+              ],
+            ),
+          )),
+    );
+  }
+
+  void increamentCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int counter = (prefs.getInt('counter') ?? 0) + 1;
+    _counter = counter; //update global variable
+    print('pressed $counter times');
+    await prefs.setInt('counter', counter); //update pref value
+    //update the UI
+    setState(() {});
+  }
+
+  Future<int> _getCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int counter = (prefs.getInt('counter') ?? 0);
+    return counter;
+  }
+}
